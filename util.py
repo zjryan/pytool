3e6c2bbbd485bda0b92fb3bf4945da9c00b9ece7
Zhou jingran <zhoujingran@linkingcloud.com>
2018-01-10 14:39:34 +0800

    time class

--- util.py
+++ util.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import time
 
+try:
+    basestring
+except NameError:
+    basestring = str
+
 
 # 转换格式为'2016-09-26 10:10:20'的时间为时间戳
 def timestamp_helper(datetime, format="%Y-%m-%d %H:%M:%S"):
@@ -16,3 +21,25 @@ def datetime_helper(timestamp, format="%Y-%m-%d %H:%M:%S"):
     local_time = time.localtime(timestamp)
     time_str = time.strftime(format, local_time)
     return time_str
+
+
+class Time(object):
+    default_format = "%Y-%m-%d %H:%M:%S"
+    def __init__(self, time, fmt=None):
+        if fmt:
+            self.default_format = fmt
+        if isinstance(time, (int, float)):
+            if time < 0:
+                raise ValueError('init param time except be a utc timestamp')
+            self.timestamp = time
+            self.datetime = datetime_helper(time, self.default_format)
+        elif isinstance(time, basestring):
+            self.timestamp = timestamp_helper(time, self.default_format)
+            self.datetime = time
+        else:
+            raise ValueError('init param time except be a utc timestamp or'
+                             'a datetime string, got {}'.format(time))
+
+
+def diff_set(s1, s2):
+    return s1.difference(s2).union(s2.difference(s1))
